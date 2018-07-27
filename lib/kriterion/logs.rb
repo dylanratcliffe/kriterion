42ab375d22d8aa1b4684962ef35cb1b6918178ce
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-07-27 10:48:12 +1000

    Added metrics collection

--- lib/kriterion/logs.rb
+++ lib/kriterion/logs.rb
@@ -2,11 +2,12 @@ require 'logger'
 
 module Kriterion::Logs
   DEBUG = ::Logger::DEBUG
+  INFO  = ::Logger::INFO
 
   def logger
     unless $logger
       $logger       = ::Logger.new(STDOUT)
-      $logger.level = ::Logger::WARN
+      $logger.level = ::Logger::INFO
     end
     $logger
   end
